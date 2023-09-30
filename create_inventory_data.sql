384db5e4698dd12a99ea46c5c19e69f7e52fcbca
Christopher Vu <christophervu03@gmail.com>
2023-09-30 18:14:24 -0500

    commands to create tables/schema

---
+++ create_inventory_data.sql
@@ -0,0 +1,17 @@
+CREATE TABLE aggregated_inventory_data (
+    Date VARCHAR(255),
+    Tapioca_Pearls_Boba INT,
+    Lychee_Jelly INT,
+    Aloe_Vera_Bits INT,
+    Grass_Jelly INT,
+    Fresh_Milk INT,
+    Red_Beans INT,
+    Cups_Regular INT,
+    Straws_Regular INT,
+    Straws_Jumbo INT,
+    Napkins_Regular INT,
+    To_Go_Bags_Small INT,
+    Lids_Dome INT,
+    Lids_Flat INT,
+    Condiment_Station_Supplies INT
+);
---
+++ create_sales_data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE sales_data (
+    month INT,
+    day INT,
+    year INT,
+    drink VARCHAR(255),
+    number_of_drinks_sold INT,
+    sales FLOAT
+);
