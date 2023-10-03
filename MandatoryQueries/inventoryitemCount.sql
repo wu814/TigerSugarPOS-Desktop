e6c1cf6f743269d76a4af38281ef3af1071b273b
Christopher Vu <christophervu03@gmail.com>
2023-10-03 15:13:36 -0500

    adding 4 offset

--- MandatoryQueries/inventoryitemCount.sql
+++ MandatoryQueries/inventoryitemCount.sql
@@ -1,5 +1,4 @@
 /* Counting number of items in the inventory, offset by 4 due to year, month, year, and hour columns */ 
 SELECT COUNT(*) - 4 AS column_count
 FROM information_schema.columns
-WHERE table_name = 'aggregated_inventory_data'
-OFFSET 4;
+WHERE table_name = 'aggregated_inventory_data';
