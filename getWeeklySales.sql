c57940f9aaa1bb02feb643acfba91e14480d8ba7
Christopher Vu <christophervu03@gmail.com>
2023-10-03 23:06:27 -0500

    removed unnecessary files

--- getWeeklySales.sql
+++
@@ -1,6 +0,0 @@
-/*Display weekly sales*/
-
-SELECT SUM(sales) AS total_sales
-FROM sales_data
-WHERE (day = 1 OR day = 2 OR day = 3 OR day = 4 OR day = 5 or day = 6 OR day = 7) AND month = 6 AND year = 2024;
-
