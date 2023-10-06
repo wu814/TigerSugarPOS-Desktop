760422b8ceb20a52ffa7188513ff7d77b2aaf57b
Christopher Vu <christophervu03@gmail.com>
2023-10-06 11:30:08 -0500

    modifying file names/comments

--- OptionalQueries/emailAndCustomerIdForSpecificCustomer.sql
+++ OptionalQueries/emailAndCustomerIdForSpecificCustomer.sql
@@ -1,4 +1,4 @@
-/*Display email and customer id for a specific person*/
+/*Display email and customer id for a specific person given first name and last name*/
 SELECT first_name, last_name, customer_id, email
 FROM customers
 WHERE first_name = 'Alice' AND last_name = 'Johnson';
