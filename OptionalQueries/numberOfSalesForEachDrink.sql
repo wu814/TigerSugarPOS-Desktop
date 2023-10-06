4215cc0484e975e03eed3f648063ad1d075d35f5
tlong36 <113958217+tlong36@users.noreply.github.com>
2023-10-05 22:02:51 -0500

    deleting old queries

--- OptionalQueries/numberOfSalesForEachDrink.sql
+++
@@ -1,7 +0,0 @@
-/*Display number of sales for all drinks in a year*/
-
-SELECT year, drink, SUM(number_of_drinks_sold) AS total_drinks_sold
-FROM sales_data
-WHERE Year = 2024
-GROUP BY year, drink
-ORDER BY total_drinks_sold DESC;
