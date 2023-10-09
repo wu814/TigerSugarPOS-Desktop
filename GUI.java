2f271da5f13144a095878dbc89b9661d658c1a56
JoshHare <67011220+JoshHare@users.noreply.github.com>
2023-10-09 16:39:10 -0500

    GUI Dev 3: JTable Implemenation
    Used Jtable to display tables.  Tables are in recent orders section (shows 10 last orders), and inventory section.  Also revised layout on above sections.

--- GUI.java
+++ GUI.java
@@ -1,7 +1,12 @@
 import java.sql.*;
+import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+
 import java.util.*;
 
 
@@ -13,12 +18,14 @@ public class GUI extends JFrame implements ActionListener {
     static JFrame inventoryFrame;
     static JFrame managerFrame;
     static JFrame cashierFrame;
+    static JFrame recentFrame;
     static JFrame currFrame; //the current framethat is being used.
     static GUI gui;
     static JPanel p; 
     static JTextArea hello; //text area for testing
     static JComboBox<Employee> employeeSelector; //drop down for employees, how we know to go in cashier view or  manager view
     static JButton employeeEnter;//locks in combobox entry
+
 
     //establishes connection to the database, through the conn variable
     public static void connect(){
@@ -68,7 +75,7 @@ public class GUI extends JFrame implements ActionListener {
       currFrame = startFrame;
       currFrame.setVisible(true);
     }
-
+    //CREATES THE INVENTORY FRAME AND READS IN FROM DATABASE
     public static void setUpInventory(){
         //frame setup
         inventoryFrame = new JFrame("Inventory");
@@ -77,30 +84,136 @@ public class GUI extends JFrame implements ActionListener {
         inventoryFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         inventoryFrame.add(inventoryPanel);
 
+        JPanel titlePanel = new JPanel();
+        titlePanel.setPreferredSize(new Dimension(1000,50));
+        JLabel title = new JLabel("Inventory");
+        titlePanel.add(title);
+
+        JPanel menuPanel = new JPanel();
+        menuPanel.setPreferredSize(new Dimension(1000,50));
+
+        inventoryFrame.add(titlePanel,BorderLayout.NORTH);
+        inventoryFrame.add(menuPanel,BorderLayout.CENTER);
+        inventoryFrame.add(inventoryPanel,BorderLayout.SOUTH);
+
         JButton backToManager = new JButton("Back to Manager Menu"); //goes back to manager menu
         backToManager.addActionListener(gui);
-        inventoryPanel.add(backToManager);
+        menuPanel.add(backToManager);
+
+        JTable table = new JTable();
+        JScrollPane  scroll = new JScrollPane(table);
+        inventoryPanel.add(scroll);
 
         //getting the data
-        String supply = "";
-        String stock = "";
         try{
           Statement stmt = conn.createStatement();
           ResultSet result = stmt.executeQuery("SELECT * FROM inventory;");
+
+          //get column names
+          int cols = result.getMetaData().getColumnCount();
+          Vector<String> colNames = new Vector<>();
+          for(int i = 1;i<=cols;i++){
+            colNames.add(result.getMetaData().getColumnName(i));
+          }
+
+          //get data
+          Vector<Vector<Object>> data = new Vector<>();
           while (result.next()) { //initializes employees with info from database, adds to vector
-              supply += result.getString("supply") + '\n';
-              stock +=result.getString("stock_remaining")+ '\n';
-        }
+              Vector<Object> row = new Vector<>();
+              for(int i = 1;i<=cols;i++){
+                row.add(result.getObject(i));
+              }
+              data.add(row);
+      
+          }
+          DefaultTableModel model = new DefaultTableModel(data,colNames);
+          table.setModel(model);
         
         } catch (Exception e){ //errors connecting to database
           JOptionPane.showMessageDialog(null,e);
         }
-        JTextArea supplies = new JTextArea(supply);
-        JTextArea stocks = new JTextArea(stock);
-        inventoryPanel.add(supplies);
-        inventoryPanel.add(stocks);
+        inventoryFrame.pack();
+
     }
-      public static void updateInventory(String ingredient){
+
+    //CREATES Recent orders Frame
+    public static void setUpRecentOrders(){
+        recentFrame = new JFrame("RecentOrders");
+        recentFrame.setSize(1000, 800);
+
+        JPanel titlePanel = new JPanel();
+        titlePanel.setPreferredSize(new Dimension(1000,50));
+        JLabel title = new JLabel("Recent Orders");
+        titlePanel.add(title);
+
+        JPanel menuPanel = new JPanel();
+        menuPanel.setPreferredSize(new Dimension(1000,50));
+
+        JPanel recentPanel = new JPanel();
+        recentFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        recentFrame.add(titlePanel,BorderLayout.NORTH);
+        recentFrame.add(menuPanel,BorderLayout.CENTER);
+        recentFrame.add(recentPanel,BorderLayout.SOUTH);
+
+        JButton backToManager = new JButton("Back to Manager Menu"); //goes back to manager menu
+        backToManager.addActionListener(gui);
+        menuPanel.add(backToManager);
+
+
+        JTable table = new JTable();
+        JScrollPane  scroll = new JScrollPane(table);
+        recentPanel.add(scroll);
+
+        //getting the data
+        try{
+          Statement stmt = conn.createStatement();
+          ResultSet result = stmt.executeQuery("SELECT * FROM orders\r\n" + //
+              "ORDER BY order_id DESC\r\n" + //
+              "LIMIT 10;");
+
+          //get column names
+          int cols = result.getMetaData().getColumnCount();
+          Vector<String> colNames = new Vector<>();
+          for(int i = 1;i<=cols;i++){
+            colNames.add(result.getMetaData().getColumnName(i));
+          }
+
+          //get data
+          Vector<Vector<Object>> data = new Vector<>();
+          while (result.next()) { //initializes employees with info from database, adds to vector
+              Vector<Object> row = new Vector<>();
+              for(int i = 1;i<=cols;i++){
+                row.add(result.getObject(i));
+              }
+              data.add(row);
+      
+          }
+          DefaultTableModel model = new DefaultTableModel(data,colNames);
+          table.setModel(model);
+
+          TableColumn column = table.getColumnModel().getColumn(0);
+          column.setPreferredWidth(200);
+          column = table.getColumnModel().getColumn(1);
+          column.setPreferredWidth(400);
+          column = table.getColumnModel().getColumn(2);
+          column.setPreferredWidth(200);
+          column = table.getColumnModel().getColumn(3);
+          column.setPreferredWidth(200);
+          column = table.getColumnModel().getColumn(4);
+          column.setPreferredWidth(900);
+          column = table.getColumnModel().getColumn(5);
+          column.setPreferredWidth(200);
+          table.setPreferredScrollableViewportSize(new Dimension(800, 400)); // Adjust the width and height as needed
+        
+        } catch (Exception e){ //errors connecting to database
+          JOptionPane.showMessageDialog(null,e);
+        }
+
+        recentFrame.pack();
+    }
+
+      //DECREAES STOCK BY 1 FOR AN INGREDIENT
+    public static void updateInventory(String ingredient){
         String query = "UPDATE inventory SET stock_remaining = stock_remaining - 1 WHERE supply = " + ingredient + ";";
         try{
           Statement stmt = conn.createStatement();
@@ -108,7 +221,11 @@ public class GUI extends JFrame implements ActionListener {
         } catch (Exception e){ //errors connecting to database
           JOptionPane.showMessageDialog(null,e);
         }
-      }
+    }
+
+    public static Timestamp getCurrentTime(){
+          return new Timestamp(System.currentTimeMillis());
+    }
 
 
 
@@ -168,15 +285,16 @@ public class GUI extends JFrame implements ActionListener {
       //Setup Start Frame
       startFrameSetup();
       setUpInventory();
+      setUpRecentOrders();
      
       
       //closing the connection
-      try {
-        conn.close();
-        //JOptionPane.showMessageDialog(null,"Connection Closed.");
-      } catch(Exception e) {
-       // JOptionPane.showMessageDialog(null,"Connection NOT Closed.");
-      }
+      // try {
+      //   conn.close();
+      //   //JOptionPane.showMessageDialog(null,"Connection Closed.");
+      // } catch(Exception e) {
+      //  // JOptionPane.showMessageDialog(null,"Connection NOT Closed.");
+      // }
     }
 
     // if button is pressed
@@ -203,6 +321,9 @@ public class GUI extends JFrame implements ActionListener {
         }
         if(event.equals("Order Statistics")){
           System.out.println("asdf");
+        }
+        if(event.equals("Recent Orders")){
+          changeFrame(recentFrame);
         }
         
       
@@ -234,6 +355,10 @@ public class GUI extends JFrame implements ActionListener {
         JButton orderStats = new JButton("Order Statistics"); //open order stats
         orderStats.addActionListener(gui);
         managerPanel.add(orderStats);
+
+        JButton recentOrders = new JButton("Recent Orders"); //open recent orders
+        recentOrders.addActionListener(gui);
+        managerPanel.add(recentOrders);
 
          //switch frame
         changeFrame(managerFrame);
